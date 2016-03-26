42e126dc016072471809d9731eb24b4893a42a1c
Ashish Joshi <ashish1995joshi@gmail.com>
2016-03-27 02:40:55 +0530

    adding more r code

--- chapter-8/stockMkt.R
+++ chapter-8/stockMkt.R
@@ -13,4 +13,17 @@ summary(glm.fit)$coef
 
 #data division
 set.seed(2)
-train.indexSMKT = sample() 
+train.indexSMKT = sample(1:nrow(Smarket),1000)
+test.dataSMKT = Smarket[-train.indexSMKT,]
+glm.fit1 = glm(Direction~Lag1+Lag2+Lag3+Lag4+Lag5+Volume,family = binomial,data=Smarket,subset = train.indexSMKT)
+logistic = predict(glm.fit,data.frame(test.dataSMKT),type = "response")
+plot(logistic,test.dataSMKT$Direction)
+pred = rep("Down",length(logistic))
+pred[logistic>.5] = "Up"
+#pred = data.frame(logistic,rep("Down",length(logistic)))
+table(pred,test.dataSMKT$Direction)
+
+library (MASS)
+lda.fit=lda(Direction~Lag1+Lag2 ,data=Smarket ,subset=train.indexSMKT)
+
+qda.fit=qda(Direction~Lag1+Lag2 ,data=Smarket ,subset=train.indexSMKT) 
